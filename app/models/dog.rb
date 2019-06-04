a337484270d91a363904ffa514e10b61ee2ad0a2
Ashley Bland <ashleyjbland@gmail.com>
2019-06-04 22:47:03 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,13 @@ class Dog
   attr_accessor :name, :age, :breed
   @@all = []
 
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    save
+  end
+
   def self.all
     @@all
   end
